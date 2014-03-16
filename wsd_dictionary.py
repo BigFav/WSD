e1d00d9cb2d37406093f513357f5a37ddb9a4420
Favian Contreras <fnc4@cornell.edu>
2014-03-16 00:06:04 -0400

    Had an 'off by 1' error in the sense mapping, and added some if statements so that we don't do the mapping shit if there is a sense number given; its back down to 8 minutes :D

--- wsd_dictionary.py
+++ wsd_dictionary.py
@@ -17,6 +17,10 @@ use_sentences = False
 lmtz_context = True
 
 
+#TODO Discount definition-less words, fix POS tag, and regex / and ancronyms
+
+
+
 def str_format(txt):
     """Removes punctuation, makes lowercase, and lemmatizes."""
     txt = re.sub("''", " ", txt)
@@ -37,30 +41,39 @@ def get_definitions(prev_token, def_num, tag):
         if not word_defs:
             for syn in wn.synsets(word):
                 word_defs.append((0,str_format(syn.definition)))
+        
+        return word_defs
     else:
         # if we know what sense it is
         if def_num:
-            wordnet_nums = [(def_num,filter(bool, dictionary[prev_token][def_num][0]))]
-        else:
-            wordnet_nums = []
-            for i,sense in enumerate(dictionary[prev_token].iterkeys()):
-                wordnet_nums.append((i,dictionary[prev_token][sense][0]))
-            wordnet_nums = sorted([(i,define) for (i,define) in wordnet_nums if define[0]], \
-                           key=itemgetter(0))
-
-        # if there is at least one wordnet definition
-        if len(wordnet_nums):
-            for (sense,wordnet_lst) in wordnet_nums:
-                for wordnet_num in wordnet_lst:
-                    defin = wn.synset("%s.%s.%s" % (word, tag, wordnet_num)).\
-                            definition
-                    word_defs.append((sense, str_format(defin)))
-        else:
-            if def_num:
+            wordnet_nums = filter(bool, dictionary[prev_token][def_num][0])
+            if wordnet_nums:
+                for wordnet_num in wordnet_nums:
+                    word_defs.append(str_format(wn.synset("%s.%s.%s" % (word, tag, wordnet_num)).definition))
+                
+                return word_defs
+            else:
                 word_defs = dictionary[prev_token][def_num][1]
                 if use_sentences:
                     word_defs += ' ' + dictionary[prev_token][def_num][2]
-                word_defs = [(def_num,str_format(word_defs))]
+                word_defs = [str_format(word_defs)]
+            
+                return word_defs
+        else:
+            wordnet_nums = []
+            for i,sense in enumerate(dictionary[prev_token].iterkeys()):
+                wordnet_nums.append((i+1,dictionary[prev_token][sense][0]))
+            wordnet_nums = sorted([(i,define) for (i,define) in wordnet_nums if define[0]], \
+                           key=itemgetter(0))
+
+            # if there is at least one wordnet definition
+            if len(wordnet_nums):
+                for (sense,wordnet_lst) in wordnet_nums:
+                    for wordnet_num in wordnet_lst:
+                        defin = wn.synset("%s.%s.%s" % (word, tag, wordnet_num)).\
+                                definition
+                        word_defs.append((sense, str_format(defin)))
+                return word_defs
             else:
                 word_defs = []
                 if use_sentences:
@@ -71,14 +84,14 @@ def get_definitions(prev_token, def_num, tag):
                     for sense in dictionary[prev_token].iterkeys():
                         word_defs.append((sense,dictionary[prev_token][sense][1]))
             
-    """ Can be optimized and/or removed
-    if rem_stop:
-        #for i,word_def in enumerate(word_defs):
-        #    word_def = word_def.split()
-        #    word_defs[i] = ' '.join([w for w in word_def if w not in stopwords])
-        word_defs = [(' '.join([w for w in word_def.split() if w not in stopwords])) for word_def in word_defs]
-    """
-    return word_defs
+                """ Can be optimized and/or removed
+                if rem_stop:
+                    #for i,word_def in enumerate(word_defs):
+                    #    word_def = word_def.split()
+                    #    word_defs[i] = ' '.join([w for w in word_def if w not in stopwords])
+                    word_defs = [(' '.join([w for w in word_def.split() if w not in stopwords])) for word_def in word_defs]
+                """
+                return word_defs
 
 def get_context_defs(word, tag):
     if tag:
@@ -130,17 +143,23 @@ for i,token in enumerate(tokens):
             target = tokens.pop(i-1)
             def_num = int(tokens.pop(i))
             tag = target[-1]
+
             if not target in target_defs.iterkeys():
                 target_defs[target] = defaultdict(list)
                 word_defs = get_definitions(target, def_num, tag)
-                for sense,word_def in word_defs:
-                    target_defs[target][sense].append(word_def)
-            
+                if def_num:
+                    target_defs[target][def_num] = word_defs
+                else:
+                    for sense,word_def in word_defs:
+                        target_defs[target][sense].append(word_def)
             elif not def_num in target_defs[target].iterkeys():
                 word_defs = get_definitions(target, def_num, tag)
-                for sense,word in word_defs:
-                    target_defs[target][sense].append(word_def)
-
+                if def_num:
+                    target_defs[target][def_num] = word_defs
+                else: 
+                    for sense,word in word_defs:
+                        target_defs[target][sense].append(word_def)
+            
             # since I messed up the indices, it auto-skips the 2nd '|'
     # token is keyword in sentence
     if '____' in token:
@@ -163,6 +182,7 @@ for i,token in enumerate(tokens):
                     context.append(lmtzr.lemmatize(tokens[i+j]))
                 else:
                     context.append(tokens[i+j]) 
+        
         # Get definitions for context words with weak POS-tag
         for i,word in enumerate(context):
             tag = nltk.pos_tag(word)[0][1].lower()
